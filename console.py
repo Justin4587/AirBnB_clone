45ae712e1b106e5125df89dcc293ea90d1379618
Justin4587 <Thurman@atlasok.com>
2021-02-19 11:07:21 -0600

    asdf

--- console.py
+++ console.py
@@ -5,4 +5,16 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """I guess I'm doing things"""
-    
+    prompt = '(hbnb)'
+
+    def emptyline(self):
+        pass
+
+    def do_EOF(self, line):
+        return True
+
+    def do_quit(self, args):
+        return True
+
+    if __name__ == '__main__':
+        HBNBCommand().cmdloop()
--- models/base_model.py
+++ models/base_model.py
@@ -16,16 +16,16 @@ class BaseModel:
                     self.id = kwargs[k]
                 elif k == "created_at":
                     self.created_at = datetime.strptime(kwargs[k], "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    if k == "updated_at":
-                        self.updated_at = datetime.strptime(kwargs[k], "%Y-%m-%dT%H:%M:%S.%f")
+                elif k == "updated_at":
+                    self.updated_at = datetime.strptime(kwargs[k], "%Y-%m-%dT%H:%M:%S.%f")
         else:
             self.id = str(uuid.uuid4())
-            
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+    
     def to_dict(self):
         """ something something darkside"""
         my_dict = dict(self.__dict__).copy()
-        my_dict.update(self.__dict__)
 
         for k in my_dict:
             if k == "id":
@@ -34,8 +34,8 @@ class BaseModel:
                 my_dict[k] = self.created_at.isoformat()
             elif k == "updated_at":
                 my_dict[k] = self.updated_at.isoformat()
-            
-        my_dict["__class__"] = self
+        
+        my_dict["__class__"] = self.__class__.__name__
 
         return my_dict
 
